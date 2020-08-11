8a5fe06bbdadc73d5a46378a743a8a91e33099da
alec dye <alecdye@gmail.com>
2020-08-11 15:45:30 -0700

    Add comments

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,12 @@
+"""
+* The input will not be empty.
+* There are no cycles in the input.
+* There are no "repeated" ancestors – if two individuals are connected, it is by exactly one path.
+* IDs will always be positive integers.
+* A parent may have any number of children.
+"""
+
 
 def earliest_ancestor(ancestors, starting_node):
+
     pass
